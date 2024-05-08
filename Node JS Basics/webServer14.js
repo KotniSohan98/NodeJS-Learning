bd3fbc61ffe1ed75c95c4a1dce0ce58cc8fa936a
KotniSohan98 <kotnisohan09@gmail.com>
2024-05-08 17:41:48 +0530

    lecture 22-23

---
+++ Node JS Basics/webServer14.js
@@ -0,0 +1,44 @@
+//Understanding Streams in Practice.
+const protocol = require("http");
+const fs = require("fs");
+
+const server = protocol.createServer();
+//Solution 1: Without Readable or writable streams
+// server.on("request", (req, res) => {
+//   fs.readFile("./Files/large-file.txt", (err, data) => {
+//     if (err) {
+//       res.end("Something went wrong!");
+//       return;
+//     }
+//     res.end(data);
+//   });
+// });
+
+//Solution 2: Using Readable and Writable stream
+
+server.on("request", (req, res) => {
+  //   fs.readFile("./Files/large-file.txt", (err, data) => {
+  //     if (err) {
+  //       res.end("Something went wrong!");
+  //       return;
+  //     }
+  //     res.end(data);
+  //   });
+  let rs = fs.createReadStream("./Files/large-file.txt");
+  rs.on("data", (chunk) => {
+    // console.log("Reading Data");
+    res.write(chunk);
+    res.write("----------------------------------------------------");
+  });
+  rs.on("end", () => {
+    res.end();
+    console.log("Response End");
+  });
+  rs.on("error", (error) => {
+    res.end(error.message);
+  });
+});
+
+server.listen(5000, () => {
+  console.log("Server Started!");
+});
