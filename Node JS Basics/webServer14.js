c26d3433ea3f45638476cbd72744f9e0d73749dd
KotniSohan98 <kotnisohan09@gmail.com>
2024-05-08 23:31:56 +0530

    lecture 24

--- Node JS Basics/webServer14.js
+++ Node JS Basics/webServer14.js
@@ -16,27 +16,27 @@ const server = protocol.createServer();
 
 //Solution 2: Using Readable and Writable stream
 
+// server.on("request", (req, res) => {
+//
+//   let rs = fs.createReadStream("./Files/large-file.txt");
+//   rs.on("data", (chunk) => {
+//     // console.log("Reading Data");
+//     res.write(chunk);
+//     res.write("----------------------------------------------------");
+//   });
+//   rs.on("end", () => {
+//     res.end();
+//     console.log("Response End");
+//   });
+//   rs.on("error", (error) => {
+//     res.end(error.message);
+//   });
+// });
+
+//Solution 3: Using Pipe method
 server.on("request", (req, res) => {
-  //   fs.readFile("./Files/large-file.txt", (err, data) => {
-  //     if (err) {
-  //       res.end("Something went wrong!");
-  //       return;
-  //     }
-  //     res.end(data);
-  //   });
   let rs = fs.createReadStream("./Files/large-file.txt");
-  rs.on("data", (chunk) => {
-    // console.log("Reading Data");
-    res.write(chunk);
-    res.write("----------------------------------------------------");
-  });
-  rs.on("end", () => {
-    res.end();
-    console.log("Response End");
-  });
-  rs.on("error", (error) => {
-    res.end(error.message);
-  });
+  rs.pipe(res);
 });
 
 server.listen(5000, () => {
